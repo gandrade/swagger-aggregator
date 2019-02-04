6a673e6c9dbc1de5e8f3b2af0a68bbf843949e29
Gustavo Andrade <gustavo.andrade@adp.com>
2019-02-04 19:52:14 -0200

    Adding greeting underlying service

---
+++ greeting-service/src/main/java/io/github/gandrade/greetingservice/GreetingServiceApplication.java
@@ -0,0 +1,14 @@
+package io.github.gandrade.greetingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingServiceApplication.class, args);
+	}
+
+}
+
---
+++ greeting-service/src/test/java/io/github/gandrade/greetingservice/GreetingServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package io.github.gandrade.greetingservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GreetingServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
